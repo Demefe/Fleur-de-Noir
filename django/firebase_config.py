44f35c570b8703c5a9666cf6af2f669904f55051
Demefe <davisnascimento09@gmail.com>
2026-05-28 05:22:24 -0300

    fix: corrige caminho do firebase.json

--- django/firebase_config.py
+++ django/firebase_config.py
@@ -1,9 +1,9 @@
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
+import os
 
-cred = credentials.Certificate('firebase.json')
-
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+cred = credentials.Certificate(os.path.join(BASE_DIR, 'firebase.json'))
 firebase_admin.initialize_app(cred)
-
 db = firestore.client()
